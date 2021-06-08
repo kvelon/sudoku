61dd58e9ac204d73f8c15d567fa20ab0adc1c80d
Kelvin <fltkelvin@u.yale-nus.edu.sg>
2021-06-08 17:56:02 +0800

    First commit. Added print and checking functions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# sudoku
-Solving sudoku with backtracking in OCaml
+# Sudoku Solver
+This is a project with the goal of implementing an algorithm to solve Sudoku puzzles in OCaml.
---
+++ lib/Util.ml
@@ -0,0 +1,49 @@
+(*
+This file is part of teaching material of Yale-NUS College module
+"YSC2229: Introductory Data Structures and Algorithms"
+
+Copyright (c) 2021 Ilya Sergey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*)
+
+(*********************************************)
+(*     Useful functions and data types       *)
+(*********************************************)
+
+(* Raise an error with a message m *)
+
+let error m = Failure m |> raise
+
+(* Extract a value from an option *)
+
+let get_exn o = match o with
+  | Some e -> e
+  | _ -> error "Empty option!"
+
+let is_min ls min = 
+  List.for_all (fun e -> min <= e) ls
+
+let print_offset _ = 
+  Printf.printf "  "
+
+let inc_cmp x y = 
+  if x > y then 1
+  else if x < y then -1
+  else 0
